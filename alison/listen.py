8d165b4949ff0116663b73c5bdbfaf1458fb3559
ConstanceGay <constancegaycsi@gmail.com>
2019-03-29 15:00:25 +0100

    Took away padding

--- alison/listen.py
+++ alison/listen.py
@@ -11,18 +11,18 @@ NUM = 1
 LEN_DATA = int(LEN_AUDIO * RATE * 2)
 
 def task():
-    print("Setting up microphone")
     mic= Microphone()
-    print("Microphone set up")
     while 1:
         print("Listening\n")
         #pixels.listen()
         data=mic.listen(1,0.5)
         data=b''.join(data)
-        treat_data(data)
+        #treat_data(data)
+        record(data)
 
+""" FUNCTION TO PAD DATA TO HAVE A CONSTANT SIZE
+def treat_data(data):               
 
-def treat_data(data):             #treats the data depending on its size
     last_index = len(data)-1        
     if len(data) == LEN_DATA:       #if the data is the right size
         print("Data is the right size "+str(len(data))+"\n")
@@ -38,10 +38,12 @@ def treat_data(data):             #treats the data depending on its size
         data2 = data[0:(LEN_DATA-1)]
         data = data[LEN_DATA:last_index]
         record(data2)
-        treat_data(data)
-            
+        treat_data(data) 
+"""
+
 def record(data):
     global NUM
+    
     if NUM < 10:
         name = '000'+str(NUM)
     elif NUM<100:
@@ -56,18 +58,9 @@ def record(data):
     f.writeframes(data)
     f.close
     NUM = NUM +1
-    size = len(data)
-    print("the length of data is "+str(size)+"\n")
-    frames = f.getnframes()
-    print("there are "+str(frames)+" frames\n")
-    rate=16000
-    duration= frames / float(rate)
-    print("the duration is "+str(duration))
-
+    
 def main():
     print('quit event')
-    print("The length of the data must be : "+str(LEN_DATA)+"\n")
-    print("NUM is : "+str(NUM)+"\n")
     while True:
         try:
             task()
