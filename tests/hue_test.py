e9e5fc74603b62613ed3da4118a1e8323f3e051b
ConstanceGay <constancegaycsi@gmail.com>
2019-05-14 18:57:35 +0200

    Added philips_hue control and tests of different modes

---
+++ tests/hue_test.py
@@ -0,0 +1,23 @@
+from philips_hue import * 
+
+def main():
+
+    try:
+    
+        print("Testing mode 1\n")
+        turn_on_alert(2,1)
+
+        print("Testing mode 2\n")
+        turn_on_alert(2,2)
+
+        print("Testing mode 3\n")
+        turn_on_alert(2,3)
+
+        print("Testing mode 4\n")
+        turn_on_alert(2,4)
+
+    except KeyboardInterrupt:
+        turn_off_led(2)
+        
+if __name__ == '__main__':
+    main()
